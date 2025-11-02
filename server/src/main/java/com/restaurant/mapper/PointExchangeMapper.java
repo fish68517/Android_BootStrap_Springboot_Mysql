d3487462bb0279c32278e4ea0c20ed2227c972ba
fish68517 <fish68517@gmail.com>
2025-11-02 23:21:47 +0800

    1

--- server/src/main/java/com/restaurant/mapper/PointExchangeMapper.java
+++
@@ -1,9 +0,0 @@
-package com.restaurant.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.restaurant.model.PointExchange;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface PointExchangeMapper extends BaseMapper<PointExchange> {
-} 
--- server/src/main/java/com/restaurant/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.restaurant.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.restaurant.model.User;
-
-public interface UserService extends IService<User> {
-    User login(String username, String password);
-}
